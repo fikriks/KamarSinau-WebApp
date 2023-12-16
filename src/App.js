195db9a35a18dadb311c001c68fb6126a66a5b7e
fikriks <fikriks130@gmail.com>
2023-12-16 13:05:23 +0700

    chore: update app js :rocket:

--- src/App.js
+++ src/App.js
@@ -1,87 +1,66 @@
 import React from "react";
+import { AuthProvider, RequireAuth } from "react-auth-kit";
 import './styles/main.css';
 import './styles/responsive.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
-import { getUserLogged, putAccessToken } from "./utils/api";
 import HomePage from './pages/HomePage';
 import AboutUsPage from './pages/AboutUsPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import DasboardPage from "./pages/DasboardPage";
-import ModulesPage from "./pages/ModuleSdPage";
+import SdModulesPage from "./pages/SdModulesPage";
 import ProfilePage from './pages/ProfilePage';
 import ForumPage from "./pages/ForumPage";
+import SdModuleDetailPage from "./pages/SdModuleDetailPage";
+import UserPage from "./pages/UserPage";
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      authedUser: null,
-      initializing: true,
-    }
+const App = () => {
+   return (
+       <AuthProvider
+         authType={"cookie"}
+         authName={"_auth"}
+         cookieDomain={window.location.hostname}
+         cookieSecure={window.location.protocol === "https:"}
+       >
+         <Routes>
+           <Route path="/" exact element={<HomePage />} />
+           <Route path="/about" exact element={<AboutUsPage />} />
+           <Route path="/forum" element={<ForumPage />} />
+           <Route path="/register" element={<RegisterPage />} />
+           <Route path="/login" element={<LoginPage />} />
+           <Route
+             path="/dasboard"
+             element={
+               <RequireAuth loginPath={"/login"}>
+                 <DasboardPage />
+               </RequireAuth>
+             }
+           />
+           <Route
+             path="/profile"
+             element={
+               <RequireAuth loginPath={"/login"}>
+                 <ProfilePage />
+               </RequireAuth>
+             }
+           />
+           <Route
+             path="/users"
+             element={
+               <RequireAuth loginPath={"/login"}>
+                 <UserPage />
+               </RequireAuth>
+             }
+           />
+           <Route path="/sd/modules" element={<SdModulesPage />} />
+           <Route
+             path="/sd/module/:moduleId"
+             element={<SdModuleDetailPage />}
+           />
+         </Routes>
+       </AuthProvider>
+     );
   }
-
-  async onLoginSuccess({ accessToken }) {
-    putAccessToken(accessToken);
-    const { data } = await getUserLogged();
-
-    this.setState(() => {
-      return {
-        authedUser: data,
-      };
-    });
-  }
-
-  async componentDidMount() {
-    document.documentElement.setAttribute('data-theme', this.state.theme);
-    const { data } = await getUserLogged();
-    this.setState(() => {
-      return {
-        authedUser: data,
-        initializing: false
-      };
-    });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.theme !== this.state.theme) {
-      document.documentElement.setAttribute('data-theme', this.state.theme);
-    }
-  }
-
-  onLogout() {
-    this.setState(() => {
-      return {
-        authedUser: null
-      }
-    });
-
-    putAccessToken('');
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <main>
-          <Routes>
-            <Route path="/" exact element={<HomePage />} />
-            <Route path="/about" exact element={<AboutUsPage />} />
-            <Route path="/forum" element={<ForumPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dasboard" element={<DasboardPage />} />
-            <Route path="/modules" element={<ModulesPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Routes>
-        </main>
-      </div>
-    )
-
-  }
-
-}
 
 export default App;
