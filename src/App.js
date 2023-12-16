854c1ecaf6064d8e018f83c6998db736650d289f
fikriks <fikriks130@gmail.com>
2023-12-16 20:51:16 +0700

    add: module route :rocket:

--- src/App.js
+++ src/App.js
@@ -14,53 +14,68 @@ import ProfilePage from './pages/ProfilePage';
 import ForumPage from "./pages/ForumPage";
 import SdModuleDetailPage from "./pages/SdModuleDetailPage";
 import UserPage from "./pages/UserPage";
+import ModulePage from "./pages/teacher/ModulePage";
+import ModuleDetailPage from "./pages/teacher/ModuleDetailPage";
 
 const App = () => {
    return (
-       <AuthProvider
-         authType={"cookie"}
-         authName={"_auth"}
-         cookieDomain={window.location.hostname}
-         cookieSecure={window.location.protocol === "https:"}
-       >
-         <Routes>
-           <Route path="/" exact element={<HomePage />} />
-           <Route path="/about" exact element={<AboutUsPage />} />
-           <Route path="/forum" element={<ForumPage />} />
-           <Route path="/register" element={<RegisterPage />} />
-           <Route path="/login" element={<LoginPage />} />
-           <Route
-             path="/dasboard"
-             element={
-               <RequireAuth loginPath={"/login"}>
-                 <DasboardPage />
-               </RequireAuth>
-             }
-           />
-           <Route
-             path="/profile"
-             element={
-               <RequireAuth loginPath={"/login"}>
-                 <ProfilePage />
-               </RequireAuth>
-             }
-           />
-           <Route
-             path="/users"
-             element={
-               <RequireAuth loginPath={"/login"}>
-                 <UserPage />
-               </RequireAuth>
-             }
-           />
-           <Route path="/sd/modules" element={<SdModulesPage />} />
-           <Route
-             path="/sd/module/:moduleId"
-             element={<SdModuleDetailPage />}
-           />
-         </Routes>
-       </AuthProvider>
-     );
+     <AuthProvider
+       authType={"cookie"}
+       authName={"_auth"}
+       cookieDomain={window.location.hostname}
+       cookieSecure={window.location.protocol === "https:"}
+     >
+       <Routes>
+         <Route path="/" exact element={<HomePage />} />
+         <Route path="/about" exact element={<AboutUsPage />} />
+         <Route path="/forum" element={<ForumPage />} />
+         <Route path="/register" element={<RegisterPage />} />
+         <Route path="/login" element={<LoginPage />} />
+         <Route
+           path="/dasboard"
+           element={
+             <RequireAuth loginPath={"/login"}>
+               <DasboardPage />
+             </RequireAuth>
+           }
+         />
+         <Route
+           path="/profile"
+           element={
+             <RequireAuth loginPath={"/login"}>
+               <ProfilePage />
+             </RequireAuth>
+           }
+         />
+         <Route
+           path="/users"
+           element={
+             <RequireAuth loginPath={"/login"}>
+               <UserPage />
+             </RequireAuth>
+           }
+         />
+         <Route
+           path="/modules"
+           element={
+             <RequireAuth loginPath={"/login"}>
+               <ModulePage />
+             </RequireAuth>
+           }
+         />
+         <Route
+           path="/modules/:id"
+           element={
+             <RequireAuth loginPath={"/login"}>
+               <ModuleDetailPage />
+             </RequireAuth>
+           }
+         />
+         <Route path="/sd/modules" element={<SdModulesPage />} />
+         <Route path="/sd/module/:moduleId" element={<SdModuleDetailPage />} />
+       </Routes>
+     </AuthProvider>
+   );
   }
 
 export default App;
